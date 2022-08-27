1c9414f950afdae4537734cb81336e51fd186d0f
David Read <49120229+david-read-iii@users.noreply.github.com>
2022-08-26 20:32:56 -0400

    Setup action bar in UI

--- app/src/main/java/com/davidread/clothescatalog/InventoryActivity.java
+++ app/src/main/java/com/davidread/clothescatalog/InventoryActivity.java
@@ -1,10 +1,13 @@
 package com.davidread.clothescatalog;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,6 +35,41 @@ public class InventoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_inventory);
         databaseInfoTextView = findViewById(R.id.database_info_text_view);
         queryRows();
+    }
+
+    /**
+     * Callback invoked to initialize the action bar. It inflates the action bar's layout.
+     *
+     * @param menu The options menu in which you place your items.
+     * @return True to show the menu. False to hide the menu.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_inventory, menu);
+        return true;
+    }
+
+    /**
+     * Callback invoked when an action bar option is clicked. It specifies what actions to take when
+     * either option is clicked.
+     *
+     * @param item The menu item that was selected.
+     * @return False to allow normal menu processing to proceed, true to consume it here.
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_insert_dummy) {
+            Toast.makeText(this, R.string.action_insert_dummy_label, Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_delete_all) {
+            Toast.makeText(this, R.string.action_delete_all_label, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        // Superclass will handle all other clicks.
+        else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     /**
