f96527270c5e80d7450d843a6502b31520f5e320
David Read <49120229+david-read-iii@users.noreply.github.com>
2022-10-22 23:36:20 -0400

    Use new package name for project because old one was strangely taken on Google Play.

--- app/src/androidTest/java/com/davidread/clothescatalog2/database/ProductProviderTest.java
+++ app/src/androidTest/java/com/davidread/clothescatalog2/database/ProductProviderTest.java
@@ -1,4 +1,4 @@
-package com.davidread.clothescatalog.database;
+package com.davidread.clothescatalog2.database;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
